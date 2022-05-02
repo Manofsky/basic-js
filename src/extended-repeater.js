3ce83a79ce5342abf117de99fa2adfe3668616f0
Manofsky <clegrof@gmail.com>
2022-05-02 21:57:56 +0300

    feat: complete extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,30 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = '';
+  let strAddition = '';
+  let arrAddition = [];
+  let arrResult = [];
+
+  if (options.additionRepeatTimes) {
+    for (let i = 0; i < options.additionRepeatTimes; i++) {
+      arrAddition.push(String(options.addition));
+    }
+    if (options.additionSeparator) strAddition = arrAddition.join(options.additionSeparator);
+    else strAddition = arrAddition.join('|');
+  } else if (options.addition) strAddition = String(options.addition);
+
+
+  if (options.repeatTimes) {
+    for (let j = 0; j < options.repeatTimes; j++) {
+      arrResult.push(str + strAddition);
+    }
+    if (options.separator) result = arrResult.join(options.separator);
+    else result = arrResult.join('+');
+  } else result = str + strAddition;
+
+  return result
 }
 
 module.exports = {
