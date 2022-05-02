038d6dd4a10c886916b887ce9940a74c4adc311b
Manofsky <clegrof@gmail.com>
2022-05-03 02:59:48 +0300

    init: start simple-chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,17 +6,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 const chainMaker = {
   getLength() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   addLink(value) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
-  removeLink(position) {
-    if (typeof position !== 'number' || !Number.isInteger(position)) {
-      throw new Error("You can't remove incorrect link!");
-    }
+  removeLink(/* position */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   reverseChain() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   finishChain() {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 };
 
